3dab1f256c4f6ba0ba18f479cdb33f8540fd2019
LenaIT <olena.denysova@gmail.com>
2020-09-30 11:24:00 +0200

    Updates

---
+++ precache-manifest.2acf8f9c3baa848f869a662dc271665f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be53e52884d77f75ce8a121e2f5092ff",
+    "url": "/to-do-app/index.html"
+  },
+  {
+    "revision": "83f4703918ffd5cff7e9",
+    "url": "/to-do-app/static/css/main.5d67a9a3.chunk.css"
+  },
+  {
+    "revision": "205bedf8256b99467499",
+    "url": "/to-do-app/static/js/2.2159022f.chunk.js"
+  },
+  {
+    "revision": "643efbae9d6e0fbba269bfc7ee232ed1",
+    "url": "/to-do-app/static/js/2.2159022f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83f4703918ffd5cff7e9",
+    "url": "/to-do-app/static/js/main.59edde9b.chunk.js"
+  },
+  {
+    "revision": "4385e1bd226fdb470057",
+    "url": "/to-do-app/static/js/runtime-main.1f8cf036.js"
+  }
+]);
